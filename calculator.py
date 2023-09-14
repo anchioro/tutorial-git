77890d672342f53bf2bc63c79bcfe323c3d892a1
Mateus Hennich <abacateshow@gmail.com>
2023-09-14 14:22:49 -0300

    feat: incremento da função de adição

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def somar(n1: int, n2: int) -> int:
+    """
+    Função para fazer a soma de dois números.
+
+    Args:
+        n1 (int): Recebe o primeiro número.
+        n2 (int): Recebe o segundo número.
+
+    Returns:
+        Retorna a soma dos números.
+    """
+
+    return n1 + n2
