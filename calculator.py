4bc0febebb676d529fe74047e75eb9ad8392dcd3
Mateus Hennich <abacateshow@gmail.com>
2023-09-14 14:25:05 -0300

    feat: incremento da função de subtração

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def subtrair(n1: int, n2: int) -> int:
+    """
+    Função para fazer a subtração de dois números.
+
+    Args:
+        n1 (int): Recebe o primeiro número.
+        n2 (int): Recebe o segundo número.
+
+    Returns:
+        Retorna a subtração dos números.
+    """
+
+    return n1 + n2
