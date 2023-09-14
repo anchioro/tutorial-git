daa6634557b6cc93c0c333a3552c08edb99d8df6
Mateus Hennich <abacateshow@gmail.com>
2023-09-14 14:28:58 -0300

    feat: incremento da função de divisão

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def dividir(n1: int, n2: int) -> int:
+    """
+    Função para fazer a divisão de dois números.
+
+    Args:
+        n1 (int): Recebe o primeiro número.
+        n2 (int): Recebe o segundo número.
+
+    Returns:
+        Retorna a divisão dos números.
+    """
+
+    return n1 / n2
