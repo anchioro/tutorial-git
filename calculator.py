0a11fccdf9f5a4f5df3841c0122dd5d317ae7170
Mateus Hennich <abacateshow@gmail.com>
2023-09-14 14:26:47 -0300

    feat: incremento da função de multiplicação

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def multiplicar(n1: int, n2: int) -> int:
+    """
+    Função para fazer a multiplicação de dois números.
+
+    Args:
+        n1 (int): Recebe o primeiro número.
+        n2 (int): Recebe o segundo número.
+
+    Returns:
+        Retorna a multiplicação dos números.
+    """
+
+    return n1 * n2
